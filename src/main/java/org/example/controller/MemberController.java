dd493c2efc364cbd22b3143ee7862615543a042e
ljh <juhee980115@gmail.com>
2023-10-03 16:32:14 +0900

    수정

--- src/main/java/org/example/controller/MemberController.java
+++ src/main/java/org/example/controller/MemberController.java
@@ -2,20 +2,19 @@ package org.example.controller;
 
 import org.example.model.CommonResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@Controller
 @RestController("/member")
 public class MemberController {
 
-    @Autowired
-    public MemberController() {
-
-    }
+//    @Autowired
+//    public MemberController() {
+//    }
 
     @GetMapping
     public CommonResponse<Void> selectMember() throws Exception {
-
         return new CommonResponse<>();
     }
 
--- src/main/java/org/example/mapper/MemberMapper.java
+++ src/main/java/org/example/mapper/MemberMapper.java
@@ -5,10 +5,12 @@ import org.apache.ibatis.annotations.Param;
 import org.example.model.member.Member;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.Mapping;
 
 
 @Mapper
+@Component
 public interface MemberMapper {
     Member selectMemberById(@Param("memberId") String memberId) throws DataAccessException;
     void insertMember(Member member) throws DataAccessException;
