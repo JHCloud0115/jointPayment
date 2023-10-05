6ae4ac1cc35780d0e816d6cb9b366b444ede6783
ljh <juhee980115@gmail.com>
2023-10-05 22:44:10 +0900

    수정

---
+++ src/main/java/org/example/config/MyBatisConfig.java
@@ -0,0 +1,43 @@
+package org.example.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@MapperScan("org.example.mapper")
+public class MyBatisConfig {
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(@Qualifier("dataSource") DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        sqlSessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath*:mapper/**/*.xml"));
+        return sqlSessionFactoryBean.getObject();
+    }
+
+    @Bean
+    public DataSourceTransactionManager transactionManager(@Qualifier("dataSource") DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+//    @Bean
+//    public DataSource dataSource() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+//        dataSource.setUrl("jdbc:mysql://localhost:3306/payment");
+//        dataSource.setUsername("root");
+//        dataSource.setPassword("Dlwngml12!");
+//        return dataSource;
+//    }
+
+
+}
--- src/main/java/org/example/mapper/MemberMapper.java
+++ src/main/java/org/example/mapper/MemberMapper.java
@@ -6,11 +6,12 @@ import org.example.model.member.Member;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.Mapping;
 
 
 @Mapper
-@Component
+@Repository
 public interface MemberMapper {
     Member selectMemberById(@Param("memberId") String memberId) throws DataAccessException;
     void insertMember(Member member) throws DataAccessException;
