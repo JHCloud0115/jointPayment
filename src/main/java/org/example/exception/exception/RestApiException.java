7943e1dca45657824a10b443ec546fcecc8e1a71
ljh <juhee980115@gmail.com>
2023-12-13 00:20:50 +0900

    공통 예외처리 수정중

---
+++ src/main/java/org/example/exception/exception/RestApiException.java
@@ -0,0 +1,11 @@
+package org.example.exception.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.example.exception.errorCode.ErrorCode;
+
+@Getter
+@RequiredArgsConstructor
+public class RestApiException extends RuntimeException{
+    private final ErrorCode errorCode;
+}
